e7d9de9c3500e7841958e4300fbca9e6a368681e
BeLeap <beleap@beleap.dev>
2024-07-14 15:11:47 +0900

    try use chezmoi

---
+++ private_dot_config/fish/config.fish
@@ -0,0 +1,70 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  fish_vi_key_bindings
+
+  set -x EDITOR nvim
+
+  alias sofish "source ~/.config/fish/config.fish"
+  alias docker "podman"
+
+  abbr v "nvim"
+
+  abbr gst "git status"
+  abbr gsw "git switch"
+  abbr gd "git diff"
+  abbr ga "git add"
+  abbr gc "git commit -v"
+  abbr gac "git commit -av"
+  abbr gp "git push"
+  abbr gf "git fetch --prune --all"
+  abbr gl "git pull"
+  abbr gco "git checkout"
+  abbr glp "git pull --rebase && git push"
+
+  abbr k "kubectl"
+  abbr ktx "kubectx"
+  abbr kns "kubens"
+
+  abbr prmd "gh pr merge -d"
+  abbr prvw "gh pr view -w"
+  abbr prv "gh pr view"
+  abbr prcm "gh pr create --assignee @me"
+
+  abbr tf "terraform"
+
+  if type -q starship 
+    starship init fish | source
+  end
+  if type -q zoxide
+    zoxide init fish | source
+  end
+  if type -q lsd
+    alias ls "lsd --group-directories-first"
+    alias ll "lsd --group-directories-first -l"
+    alias lla "lsd --group-directories-first -la"
+  end
+
+  if type -q nodenv
+    source (nodenv init -|psub)
+  end
+
+  if type -q direnv
+    direnv hook fish | source
+  end
+
+  if type -q carapace
+    carapace _carapace | source
+  end
+
+  if type -q rbenv
+    status --is-interactive; and rbenv init - fish | source
+  end
+
+  if type -q fzf
+    fzf --fish | source
+  end
+
+  if type -q pyenv
+    status --is-interactive; and pyenv init - | source
+  end
+end
