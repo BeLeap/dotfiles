503af4623e7ce2ef23fc9ca6e7ab8cc6e6542e90
BeLeap <beleap@beleap.dev>
2024-07-15 20:55:59 +0900

    Update .config/nvim/lua/plugins/llm.lua

--- private_dot_config/nvim/lua/plugins/llm.lua
+++ private_dot_config/nvim/lua/plugins/llm.lua
@@ -17,43 +17,25 @@ return {
 		},
 		config = function()
 			local ollama = require("model.providers.ollama")
+			local starters = require("model.prompts.starters")
+			local chats = require("model.prompts.chats")
+
 			require("model").setup({
 				chats = {
-					llama = {
+					llama = vim.tbl_deep_extend("force", chats["together:codellama"], {
 						provider = ollama,
 						params = {
 							model = "llama3",
 						},
-						builder = function(input)
-							return {
-                prompt = "[INST] You are a helpful code assistant.\n"
-                  .. input
-                  .. "[/INST]",
-							}
-						end,
-					},
+					}),
 				},
 				prompts = {
-					commit = {
+					commit = vim.tbl_deep_extend("force", starters["commit"], {
 						provider = ollama,
 						params = {
 							model = "llama3",
 						},
-						mode = require("model").mode.INSERT,
-						builder = function()
-							local git_diff = vim.fn.system({ "git", "diff", "--staged" })
-
-							if not git_diff:match("^diff") then
-								error("Git error:\n" .. git_diff)
-							end
-
-							return {
-								prompt = "Write a terse commit message according to the Conventional Commits specification. Try to stay below 80 characters total. Staged git diff: ```\n"
-									.. git_diff
-									.. "\n```",
-							}
-						end,
-					},
+					}),
 				},
 			})
 		end,
