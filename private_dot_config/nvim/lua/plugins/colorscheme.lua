b01078add29e2eecee7935b79e7cc1350fd93ced
BeLeap <beleap@beleap.dev>
2024-07-28 16:03:50 +0900

    Update .config/nvim/lua/plugins/colorscheme.lua

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,6 @@ return {
   {
     "olimorris/onedarkpro.nvim",
     priority = 1000, -- Ensure it loads first
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = true,
     lazy = false,
     init = function()
       vim.o.background = "dark"
