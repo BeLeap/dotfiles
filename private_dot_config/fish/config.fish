cfa0e602a9842c8c7cc8f5d3dd1d1bc3bf9f4189
BeLeap <beleap@beleap.dev>
2024-07-30 10:13:49 +0900

    Update .config/fish/config.fish Update .config/fish/git.fish

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -12,29 +12,13 @@ if status is-interactive
 
     alias e "$EDITOR"
 
-    alias lt "ls --tree --depth 3"
+    source ~/.config/fish/git.fish
 
-    abbr gst "git status"
-    abbr gsw "git switch"
-    abbr gd "git diff"
-    abbr ga "git add"
-    abbr gc "git commit -v"
-    abbr gac "git commit -av"
-    abbr gp "git push"
-    abbr gf "git fetch --prune --all"
-    abbr gl "git pull"
-    abbr gco "git checkout"
-    abbr glp "git pull --rebase && git push"
+    alias lt "ls --tree --depth 3"
 
     abbr k kubectl
     abbr ktx kubectx
     abbr kns kubens
-
-    abbr prmd "gh pr merge -d"
-    abbr prm "gh pr merge"
-    abbr prvw "gh pr view -w"
-    abbr prv "gh pr view"
-    abbr prcm "gh pr create --assignee @me"
 
     abbr tf terraform
 
--- private_dot_config/fish/git.fish
+++ private_dot_config/fish/git.fish
@@ -1,1 +1,19 @@
-# dummy
+# git
+abbr gst "git status"
+abbr gsw "git switch"
+abbr gd "git diff"
+abbr ga "git add"
+abbr gc "git commit -v"
+abbr gac "git commit -av"
+abbr gp "git push"
+abbr gf "git fetch --prune --all"
+abbr gl "git pull"
+abbr gco "git checkout"
+abbr glp "git pull --rebase && git push"
+
+# github
+abbr prmd "gh pr merge -d"
+abbr prm "gh pr merge"
+abbr prvw "gh pr view -w"
+abbr prv "gh pr view"
+abbr prcm "gh pr create --assignee @me"
