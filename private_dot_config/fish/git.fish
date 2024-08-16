0f3c580ae57cbe3faceb53e20ef50a931a2ca874
BeLeap <beleap@beleap.dev>
2024-08-16 16:37:00 +0900

    Update .config/fish/git.fish

--- private_dot_config/fish/git.fish
+++ private_dot_config/fish/git.fish
@@ -95,6 +95,8 @@ function create_rc_pr
 end
 alias crcpr create_rc_pr
 
-function fco -d "Fuzzy-find and checkout a branch"
-    git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
+if $FZF_EXISTS
+    function fco -d "Fuzzy-find and checkout a branch"
+        git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
+    end
 end
