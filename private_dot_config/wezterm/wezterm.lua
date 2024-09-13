e849284108e319613664331725246626a365f3ff
BeLeap <beleap@beleap.dev>
2024-09-13 23:09:46 +0900

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 
 config.font = wezterm.font 'Cascadia Code'
-config.font_size = 14
+config.font_size = 17
 
 config.color_scheme = 'One Dark (Gogh)'
 
