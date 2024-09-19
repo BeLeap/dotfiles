5a45d1bc40a442be5193a97ffcc7a354b93fe980
BeLeap <beleap@beleap.dev>
2024-09-19 15:00:05 +0900

    Update .config/nvim/lua/plugins/project.lua

--- private_dot_config/nvim/lua/plugins/project.lua
+++ private_dot_config/nvim/lua/plugins/project.lua
@@ -17,7 +17,7 @@ return {
     config = function()
       require("project_nvim").setup({
         detection_methods = { "pattern", "lsp" },
-        pattern = { "root.md", ">projects" },
+        pattern = { "root.md", ">projects", ".nvim" },
         silent_chdir = true,
         show_hidden = true,
         excluder_dirs = {
