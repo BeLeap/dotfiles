39d8112ba4863f06fee32b332d968b78a25d2a99
BeLeap <beleap@beleap.dev>
2024-10-11 01:19:41 +0900

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ config.automatically_reload_config = true
 config.font = wezterm.font 'Cascadia Code'
 config.font_size = 17
 
-config.color_scheme = 'OneDark (base16)'
+config.color_scheme = 'Nord'
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
