b71e6288fb23107f6d3cd93ac4363f2f54e1dc6a
BeLeap <beleap@beleap.dev>
2024-10-14 11:53:21 +0900

    Update .config/zsh/util.zsh

--- private_dot_config/zsh/util.zsh
+++ private_dot_config/zsh/util.zsh
@@ -7,7 +7,7 @@ function set_alias_if_exists() {
   local binary=$2
   local preferred_cmd=$3
   local fallback_cmd=${4:-}
-  if [[ command_exists $binary ]]; then
+  if [[ command_exists "$binary" ]]; then
     alias "$alias_cmd"="$preferred_cmd"
   elif [[ ! -z "$fallback_cmd" ]]; then
     alias "$alias_cmd"="$fallback_cmd"
