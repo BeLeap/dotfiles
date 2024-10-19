946427be05cc164e763ef31134f4c02941084ced
BeLeap <beleap@beleap.dev>
2024-10-19 19:35:00 +0900

    Update .config/zsh/init.zsh

--- private_dot_config/zsh/init.zsh
+++ private_dot_config/zsh/init.zsh
@@ -73,35 +73,13 @@ if command_exists direnv; then
 fi
 
 if command_exists fzf; then
+  source <(fzf --zsh)
+
   # cdf - cd into the directory of the selected file
   cdf() {
      local file
      local dir
      file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
-  }
-
-  # re-wrote the script above
-  bind '"\C-r": "\C-x1\e^\er"'
-  bind -x '"\C-x1": __fzf_history';
-
-  __fzf_history ()
-  {
-    __ehc $(history | fzf --tac --tiebreak=index | perl -ne 'm/^\s*([0-9]+)/ and print "!$1"')
-  }
-
-  __ehc()
-  {
-    if
-      [[ -n $1 ]]
-    then
-      bind '"\er": redraw-current-line'
-      bind '"\e^": magic-space'
-      READLINE_LINE=${READLINE_LINE:+${READLINE_LINE:0:READLINE_POINT}}${1}${READLINE_LINE:+${READLINE_LINE:READLINE_POINT}}
-      READLINE_POINT=$(( READLINE_POINT + ${#1} ))
-    else
-      bind '"\er":'
-      bind '"\e^":'
-    fi
   }
 fi
 
