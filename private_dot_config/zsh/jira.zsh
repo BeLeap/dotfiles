3aeda70539eda75c7d4ac3d17eaf6e93a9c2377e
BeLeap <beleap@beleap.dev>
2024-10-23 10:29:18 +0900

    Update .config/zsh/jira.zsh

--- private_dot_config/zsh/jira.zsh
+++ private_dot_config/zsh/jira.zsh
@@ -2,7 +2,7 @@ if command_exists jira; then
   function create_branch_from_jira() {
     # JQL을 사용해 원하는 티켓 리스트를 가져옵니다.
     # 여기서는 현재 할당된 티켓들을 예로 들었습니다.
-    local jql_query="assignee = currentUser() AND status IN ('To Do', 'In Progress')"
+    local jql_query="assignee = currentUser() AND status IN ('To Do', 'In Progress', 'In Review')"
 
     # JIRA 티켓 리스트를 JSON 형식으로 가져옵니다.
     local jira_issues=$(jira issue list --jql "$jql_query" --plain --no-headers --columns KEY,SUMMARY | fzf)
