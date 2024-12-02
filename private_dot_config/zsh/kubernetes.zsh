6fb4ecaf758cdcd519fe58c8a83a735b6e02894b
BeLeap <beleap@beleap.dev>
2024-12-02 20:09:01 +0900

    Update .config/zsh/kubernetes.zsh

--- private_dot_config/zsh/kubernetes.zsh
+++ private_dot_config/zsh/kubernetes.zsh
@@ -2,7 +2,7 @@ export K9S_CONFIG_DIR=$HOME/.config/k9s
 
 set_alias_if_exists k kubectl-check "kubectl check" "kubectl"
 set_alias_if_exists ktx kubectx kubectxl
-set_alias_if_exists ktx kubens kubens
+set_alias_if_exists kns kubens kubens
 set_alias_if_exists ku k9s "k9s"
 set_alias_if_exists kh k9s "k9s --headless"
 
