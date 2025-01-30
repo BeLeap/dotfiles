ead73f278311455027ed1ca172698c5d671192f2
BeLeap <beleap@beleap.dev>
2025-01-31 01:14:55 +0900

    Update .config/nvim/lua/plugins/colorscheme.lua

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
-    lazy = false,
-    init = function()
-      vim.o.background = "dark"
-      vim.cmd([[colorscheme onedark]])
-    end,
-  },
+  -- {
+  --   "olimorris/onedarkpro.nvim",
+  --   priority = 1000, -- Ensure it loads first
+  --   lazy = false,
+  --   init = function()
+  --     vim.o.background = "dark"
+  --     vim.cmd([[colorscheme onedark]])
+  --   end,
+  -- },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
