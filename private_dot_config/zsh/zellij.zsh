a8645432ec46aac3057f2cda0740cc8f2b210de9
BeLeap <beleap@beleap.dev>
2025-03-02 01:07:08 +0900

    Update .config/zsh/zellij.zsh

--- private_dot_config/zsh/zellij.zsh
+++ private_dot_config/zsh/zellij.zsh
@@ -1,29 +1,29 @@
 if command_exists zellij; then
-  alias zj="zellij attach --create home"
-  alias j="z \$(zellij list-sessions -n | grep current | awk '{print \$1}' | sed 's/_/\//g')"
+    alias zj="zellij attach --create home"
+    alias j="z \$(zellij list-sessions -n | grep current | awk '{print \$1}' | sed 's/_/\//g')"
 
-  zj() {
-      if [[ -z "$1" ]]; then
-      else
-      fi
-  }
+    zj() {
+        if [[ -z "$1" ]]; then
+        else
+        fi
+    }
 
-  zellij_tab_name_update() {
-      if [[ -n $ZELLIJ ]]; then
-          local current_dir=$PWD
-          if [[ $current_dir == $HOME ]]; then
-              current_dir="~"
-          else
-              current_dir=${current_dir##*/}
-          fi
-          command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
-      fi
-  }
+    zellij_tab_name_update() {
+        if [[ -n $ZELLIJ ]]; then
+            local current_dir=$PWD
+            if [[ $current_dir == $HOME ]]; then
+                current_dir="~"
+            else
+                current_dir=${current_dir##*/}
+            fi
+            command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
+        fi
+    }
 
-  zellij_tab_name_update
-  chpwd_functions+=(zellij_tab_name_update)
+    zellij_tab_name_update
+    chpwd_functions+=(zellij_tab_name_update)
 
-  zj_cleanup() {
-      zellij list-sessions -n | grep -v current | grep -v home | awk '{print $1}' | xargs -I % zellij kill-session %
-  }
+    zj_cleanup() {
+        zellij list-sessions -n | grep -v current | grep -v home | awk '{print $1}' | xargs -I % zellij kill-session %
+    }
 fi
