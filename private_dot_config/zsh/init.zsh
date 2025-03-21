e8dc4af3d33470199eddfb20eff54758ccab6c93
BeLeap <beleap@beleap.dev>
2025-03-21 21:28:39 +0900

    Update .config/zsh/init.zsh

--- private_dot_config/zsh/init.zsh
+++ private_dot_config/zsh/init.zsh
@@ -2,8 +2,8 @@ source $ZSH_CONFIG_PATH/util.zsh
 
 autoload -Uz compinit && compinit
 
-if [[ ! -z "$(command -v hx)" ]]; then
-    export EDITOR="hx"
+if [[ ! -z "$(command -v nvim)" ]]; then
+    export EDITOR="nvim"
 else
     export EDITOR="vim"
 fi
